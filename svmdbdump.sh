c9d353196d16421692381ebc711a4280515a5e97
solusvm-support <55432094+solusvm-support@users.noreply.github.com>
2019-09-17 15:42:23 +0700

    Update svmdbdump.sh

--- svmdbdump.sh
+++ svmdbdump.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-MYSQL_PWD=`cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $3}'` mysqldump -u`cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $2}'` `cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $1}'` > /root/solusvm`date +%F_%H.%M`.sql
+
+echo "Backup of SolusVM database started ... "
+
+MYSQL_PWD=`cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $3}'` mysqldump -u`cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $2}'` `cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $1}'` > /root/solusvmdb`date +%F_%H.%M`.sql
+
+echo "Backup has been created and put into /root/ directory"
+ls -l /root/ | grep solusvmdb
