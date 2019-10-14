8561abd5b9c7bb20fcad3d3f0e0e3cb3c9d4a2cc
Aleksandr Bashurov <45628798+abashurov@users.noreply.github.com>
2019-10-15 01:54:27 +0700

    Code style fixes

--- svmdbdump.sh
+++ svmdbdump.sh
@@ -1,27 +1,48 @@
-#!/bin/bash
-# The script creates a backup of SolusVM database and puts it into /root/ directory
+#!/usr/bin/env bash
+# The script creates a backup of SolusVM database and puts it into the current directory
 # KCS article #360009293960
 
-echo "Backup of SolusVM database started... "
-sleep 1
+set -euo pipefail
 
-# checking that solusvm.conf exists
-if [[ -f /usr/local/solusvm/includes/solusvm.conf ]]; then
-    echo -e "\nVerified that file /usr/local/solusvm/includes/solusvm.conf exists. Continue...\n"
-else
-    echo -e "\n---------------------------------\n"
-    echo -e "Backup is failed to create: file /usr/local/solusvm/includes/solusvm.conf does not exists.\n"
-    echo -e "Make sure that the server is correct and SolusVM Master software is installed"
+# Creates a backup of the database and moves it to the proper location
+# Globals: MYSQL_PASS MYSQL_DB MYSQL_USER TMPNAME
+function backup() {
+    local bkp_name
+    MYSQL_PWD="${MYSQL_PASS}" mysqldump -u"${MYSQL_USER}" "${MYSQL_DB}" > "${TMPNAME}"
+    bkp_name="solusvmdb$(date +%F_%H.%M).sql"
+    mv "${TMPNAME}" "$bkp_name"
+
+    echo "Backup file /$(pwd)/$bkp_name has been created"
+}
+
+# Checks whether the MySQL credential file exists and can be read
+# Globals: +MYSQL_PASS +MYSQL_DB +MYSQL_USER +TMPNAME
+function init() {
+    [[ ! -f /usr/local/solusvm/includes/solusvm.conf ]] && \
+        err "Cannot create backup: /usr/local/solusvm/includes/solusvm.conf does not exist\n" \
+            "Make sure that the server is correct and SolusVM Master software is installed"
+    [[ ! -r /usr/local/solusvm/includes/solusvm.conf ]] && \
+        err "Cannot create backup: /usr/local/solusvm/includes/solusvm.conf cannot be read by the current user"
+
+    MYSQL_PASS="$(cut -d':' -f3 /usr/local/solusvm/includes/solusvm.conf)"
+    MYSQL_USER="$(cut -d':' -f2 /usr/local/solusvm/includes/solusvm.conf)"
+    MYSQL_DB="$(cut -d':' -f1 /usr/local/solusvm/includes/solusvm.conf)"
+
+    TMPNAME="$(mktemp /tmp/svmdump.XXXXXX)"
+
+    echo "Backup of SolusVM database has started..."
+}
+
+# Stops script execution with an error
+# Args: $* any
+function err() {
+    echo -e "$*" >&2
     exit 1
-fi
+}
 
-set -Eoe pipefail
-trap "echo "";echo Something went wrong. Backup failed to create; rm -f /root/4175078534svmdb.tmp" ERR
+function main() {
+    init
+    backup
+}
 
-MYSQL_PWD=`cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $3}'`
-mysqldump -u`cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $2}'` "-p${MYSQL_PWD}" `cat /usr/local/solusvm/includes/solusvm.conf | awk -F ":" '{print $1}'` > /root/4175078534svmdb.tmp
-
-bkp_name=$(echo solusvmdb`date +%F_%H.%M`.sql)
-mv /root/4175078534svmdb.tmp /root/$bkp_name
-
-echo "Backup file /root/$bkp_name has been created"
+main "$@"
