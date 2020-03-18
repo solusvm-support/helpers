00d0ce9667564c72339f563b78e94da5cb0c1bef
solusvm-support <55432094+solusvm-support@users.noreply.github.com>
2020-03-18 14:21:39 +0700

    Update solusnxt_chng.sh

--- solusnxt_chng.sh
+++ solusnxt_chng.sh
@@ -5,46 +5,46 @@
 set -euo pipefail
 
 function init() {
-    echo "Checking that solusnxt directories exist: \n"
+    echo "Checking that solusnxt directories exist:"
     [[ ! -d /etc/solusnxt/ ]] && \
         err "Cannot find /etc/solusnxt/ directory\n" \
             "Make sure that the server is correct and directory exists"
     [[ ! -d /usr/local/solusnxt ]] && \
         err "Cannot find /usr/local/solusnxt/ directory\n" \
             "Make sure that the server is correct and directory exists"
-	echo "PASS\n"
+	echo "PASS"
 }
 
 function move_directories() {
-	echo "Copying content from /etc/solusnxt/ to /etc/solus \n"
+	echo "Copying content from /etc/solusnxt/ to /etc/solus"
 	cp -r /etc/solusnxt/ /etc/solus
-	echo "Completed \n"
+	echo "Completed"
 	
-	echo "Removing cache from /usr/local/solusnxt/cache/ \n"
+	echo "Removing cache from /usr/local/solusnxt/cache/"
 	rm -rf /usr/local/solusnxt/cache/*
-	echo "Completed \n"
+	echo "Completed"
 	
-	echo "Copying content from /usr/local/solusnxt to /usr/local/solus \n"
+	echo "Copying content from /usr/local/solusnxt to /usr/local/solus"
 	cp -r /usr/local/solusnxt /usr/local/solus
-	echo "Completed \n"
+	echo "Completed"
 }
 
 function update_services() {
-	echo "Stopping/disabling old solusnxt-agent.service \n"
+	echo "Stopping/disabling old solusnxt-agent.service"
 	systemctl stop solusnxt-agent.service
 	systemctl disable solusnxt-agent.service
 	rm -f /etc/systemd/system/solusnxt-agent.service
 	systemctl mask solusnxt-agent.service
-	echo "Completed \n"
+	echo "Completed"
 	
-	echo "Enabling/starting solus-agent.service \n"
+	echo "Enabling/starting solus-agent.service"
 	systemctl daemon-reload
 }
 
 function main() {
     init
     move_directories
-	update_services
+    update_services
 }
 
 main "$@"
